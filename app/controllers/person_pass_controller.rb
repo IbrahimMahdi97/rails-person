8148e5e7f7a1fed9007b3f3b2c2e8674526e99b7
IbrahimMahdi97 <ibrahimmahdi115@gmail.com>
2020-12-25 22:10:10 +0300

    update controller and model

--- app/controllers/person_pass_controller.rb
+++ app/controllers/person_pass_controller.rb
@@ -2,23 +2,19 @@ class PersonPassController < ApplicationController
   def show
     binding.pry
   end
-    
-  
   
   def new
     @person_pass = PersonPass.new
   end
 
   def create
-    @person_pass = PersonPass.new(check_params)
-    if @person_pass[:email] == "ralf"  && @person_pass[:password] == "poop"
-      flash[:info] = "Correct"
-      redirect_to '/'
-    else
+    @person_pass = PersonPass.where(email: params[:person_pass][:email], username: params[:person_pass][:username], password: params[:person_pass][:password]).first
+    if @person_pass.blank? 
       flash[:warning] = "Wrong"
-      redirect_to '/'
+    else 
+      flash[:info] = "Correct"
     end
-    
+    redirect_to '/'
   end 
 
   private 
